b76733ba367a4dc429df0f1b0ce07330e22eb55e
Harry Hawkesford <harry.hawkesford@sykescottages.co.uk>
2024-02-02 16:33:20 +0000

    Hello world

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	got := Hello()
+	want := "Hello, world"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
